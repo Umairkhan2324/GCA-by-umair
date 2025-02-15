80c48eebc3c2f0e90872d57c4e67203414bb2d78
umiar khan <asadsher2324@gmail.com>
2025-02-15 16:50:18 +0500

    response format added

--- app.py
+++ app.py
@@ -11,15 +11,24 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 if not OPENAI_API_KEY:
     raise ValueError("OPENAI_API_KEY not found in environment variables")
 
-from upsonic import Task, Agent, KnowledgeBase
-from upsonic.client.tools import Search
+from upsonic import Task, Agent, KnowledgeBase, ObjectResponse
+from upsonic.client.tools import Search, ComputerUse
+
+class initial_repsonse(ObjectResponse):
+    analysis : str
 
 my_resume = KnowledgeBase(
     files = ["my_resume.pdf"]
 )
-task = Task(
-    "Research latest news in Anthropic and OpenAI", 
-    tools=[Search]
+Initial_analysis = Task(
+    "You are acting as a recruiter and going to deeply analyze and understand resume given in knowledge base and tell which job will be best for this person and will write about what type of jobs with what specifications will be best for him", 
+    context = ["my_resume"],
+    response_format = initial_repsonse
+)
+task2 = Task(
+    "You are going to understand the analysis given by task1 and search for the most suited jobs according to it across multiple platfroms.",
+    context = ["my_resume","Initial_analysis"],
+    tool = [Search,ComputerUse]
 )
 
 agent = Agent(
